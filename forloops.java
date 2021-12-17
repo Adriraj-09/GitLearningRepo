9f4c817022a2587f2d51e43f22c6a7296f2c56fa
Adriraj Mukhopadhyay <adrirajmukherjee@gmail.com>
2021-12-17 22:12:28 +0530

    Forloops executed

---
+++ forloops.java
@@ -0,0 +1,8 @@
+class forloops{
+	public static void main(String[] args) {
+		for (int i = 0; i<10 ; i++) {
+			System.out.println("Forloops executed");
+		}
+	System.out.println("Hello World");
+	}
+}
